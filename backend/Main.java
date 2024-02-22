97f14e9bcdd51de528fec6146cd628e1c82186fa
Paritosh Pranjal <paritosh.pranjal@zemosolabs.com>
2024-02-22 10:57:13 +0530

    Feature/exploring workflow (#1)

---
+++ backend/Main.java
@@ -0,0 +1,7 @@
+package backend;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("This is backend testing for github action");
+    }
+}
---
+++ frontend/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const index = () => {
+  return (
+    <div>
+        This is home   page
+    </div>
+  )
+}
