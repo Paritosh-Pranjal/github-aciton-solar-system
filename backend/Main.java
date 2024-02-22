6dc233a653200ce486c7c2f2a19a1f4c1d6916d9
Paritosh-Pranjal <paritosh.pranjal@zemosolabs.com>
2024-02-22 10:18:20 +0530

    changes in backend

--- backend/Main.java
+++ backend/Main.java
@@ -2,6 +2,6 @@ package backend;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("This is backend testing");
+        System.out.println("This is backend testing for github");
     }
 }
